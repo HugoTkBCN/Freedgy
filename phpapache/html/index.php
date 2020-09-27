59216c57fff5ad80096ac27d7473e1555543745e
Hugo <hugo1.lachkar@epitech.eu>
2020-09-27 13:49:37 +0200

    try to resolve css

--- phpapache/html/index.php
+++ phpapache/html/index.php
@@ -11,8 +11,8 @@
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="icon" type="image/ico" href="//assets/logo.png" />
-	<link href="//assets/css/main.css" rel="stylesheet" type="text/css" />
-	<link href="//assets/css/style.css" rel="stylesheet" type="text/css" />
+	<link href="assets/css/main.css" rel="stylesheet" type="text/css" />
+	<link href="assets/css/style.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
