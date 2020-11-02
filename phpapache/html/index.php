14b9b3acc241d0dfa9ddaf90592a1eb6bde96ba5
Hugo <hugolachkar@gmail.com>
2020-11-02 12:58:00 +0100

    [UPDATE] header

--- phpapache/html/index.php
+++ phpapache/html/index.php
@@ -47,6 +47,9 @@
 		</nav>
 		<a href="index.php"><img id="logo" src="assets/logo.png"></img></a>
 	</header>
+	<div class="newletter_button">
+		<a class="button" href="#newsletter">Join Our NewsLetter</a>
+	</div>
 
 	<!-- Menu -->
 	<nav id="menu">
@@ -68,9 +71,6 @@
 			</ul>
 		</div>
 	</section>
-	<div class="newletter_button">
-		<a class="button" href="#newsletter">Join Our NewsLetter</a>
-	</div>
 	<div id="newsletter" class="overlay">
 		<div class="popup">
 			<h2>Join our newsletter</h2>
@@ -96,19 +96,10 @@
 	</div>
 
 	<!-- One -->
-	<section id="one" class="wrapper">
-		<div class="inner flex flex-3">
-			<p><span class="image left"><img src="images/supermarket.jpg" /></span>Every day, many people cannot afford to buy the food necessary to have a decent diet. Today in France, there are about 250,000 people without a stable home who have to live in shelters or on the street. Every day, millions of students skip meals as a solution to a bad financial situation. Every day, some families, especially large ones, struggle to nourish every member properly. Contradictorily, every day, on average, each supermarket in France throws away 20 kilograms of food. Isn’t that a big irony?</p>
-		</div>
-		<div class="inner flex flex-3">
-
-			<div><span class="image right"><img src="assets/logo.png" /></span>
-				<p>To help solve this huge issue, we want to create something that will help people in those precarious situations as well as reduce the food waste made by unsold supermarket products. And that is why we decided to create Freedgy.</p>
-				<p>Freedgy is a non-profitable company that will spread a movement of solidarity. It is already starting to get popular in Europe and it consists in putting fridges in public areas, accessible by everyone: one can put food in the fridge as well as take food as need be.</p>
-				<p>We want to digitize that movement by creating and distributing connected fridges. The fridges have a GPS inside, and are linked with our platform which allows users to make donations easily with our intuitive and simple interface. You have the possibility to help many people thanks to a few clicks and the power of generosity. Once your donation is registered in our database, we will send the request to the nearest supermarket in charge of the fridge you chose to donate to and they will take care of putting all your gifts in the fridge. You can also choose a specific supermarket which has a partnership with us and make a donation without selecting one of our Freedgies. That is helpful if a supermarket seems to have larger amounts of waste compared to other supermarkets. The supermarket will then deposit the food in the nearest Freedgy around them. Once your gift is delivered anyone who needs it may take it.
-					We are looking forward to pushing that ambitious project to its full potential and hope we can all make a big change in the current food hunger and food waste situation. We hope you will follow us through that challenging and exciting journey.</p>
-			</div>
-		</div>
+	<section id="one" class="trailer">
+		<iframe id="ytplayer" type="text/html" width="960" height="540"
+  		src="http://www.youtube.com/embed/Q8gARryJA9g?autoplay=1">
+		</iframe>
 	</section>
 
 	<!-- Two -->
